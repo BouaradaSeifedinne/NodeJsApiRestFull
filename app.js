55ba0c96e4baff942dc429e70084e09aff2f2d17
Bouarada Seifedinne <seifscauf@gmail.com>
2018-03-06 17:35:30 +0100

    nodeJs API version alpha

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**** Import ****/
+const express = require('express')
+var bodyParser = require('body-parser')
+var MongoClient = require('mongodb').MongoClient;
+var passport = require('passport');
+var Strategy = require('passport-http-bearer').Strategy;
+
+const app = express()
+
+/**** Middleware ****/
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+/**** Routes ****/
+
+app.get('/', function (req, res) {
+  res.status(200).json({ success: 'Hello World!'});
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
